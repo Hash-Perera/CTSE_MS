87109b06108cf0254fbf961e66a112bb5f6f37f1
Hashan Perera <hashperera.v@gmail.com>
2025-04-17 13:06:16 +0530

    new endpint

--- app.js
+++ app.js
@@ -20,6 +20,9 @@ app.use(morgan("dev"));
 app.get("/", (req, res) => {
   res.send("Welcome to the CTSE API!");
 });
+app.get("/api", (req, res) => {
+  res.send("API is running...");
+});
 app.use("/api/auth", authRoutes);
 
 export default app;
