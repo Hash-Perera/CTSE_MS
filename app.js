d4909431bab26bbd33d95cf13d858813ddd09813
Hashan Perera <hashperera.v@gmail.com>
2025-04-17 17:13:35 +0530

    Update yml

--- app.js
+++ app.js
@@ -23,6 +23,10 @@ app.get("/", (req, res) => {
 app.get("/api", (req, res) => {
   res.send("API is running...");
 });
+app.get("/api/health", (req, res) => {
+  res.send("Health check OK");
+});
+
 app.use("/api/auth", authRoutes);
 
 export default app;
